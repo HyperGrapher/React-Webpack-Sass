238ec14c3050493e56d032d0b521d595015c810d
Hypergraph <3758349+xyzmes@users.noreply.github.com>
2022-02-22 05:31:01 +0300

    updated app jsx

--- src/app.jsx
+++ src/app.jsx
@@ -9,13 +9,6 @@ const App = () => {
       <h3 className={style.title}>
         React Starter Template with Webpack And Sass
       </h3>
-      <p>Medium Link:</p>
-      <a
-        className={style.link}
-        href="https://medium.com/@joshuacrass/react-scss-and-webpack-ae9bd84b9e43"
-      >
-        react-scss-and-webpack
-      </a>
     </div>
   );
 };
